e6d7ae6f00524bbd383bed53d7199fceef85c8d6
no <ericliao10797@hotmail.com>
2015-08-10 15:19:13 -0400

    lol

--- app.py
+++ app.py
@@ -7,6 +7,20 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
     
+@app.route("/details")
+def details():
+    return render_template("details.html")
+
+@app.route("/contact")
+def contact():
+    return render_templates("contact.html")
+
+@app.route("location")
+def location():
+    return render_template("location.html")
+    
+    
+    
 if __name__ == "__main__":
     app.debug=True
     app.run("0.0.0.0",port=80)
