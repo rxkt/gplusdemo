20b3a1da791047fe7888c3d6c0d1584188eb142b
rxkt <ericliao10797@hotmail.com>
2015-08-10 18:08:23 -0400

    :P

--- app.py
+++ app.py
@@ -11,19 +11,24 @@ change temapltes/* to templates/en,sp,cn/*
 @app.route("/home",methods=['GET'])
 @app.route("/",methods=['GET'])
 def home():
-    return render_template("index.html")
-    
+    if len(request.args) > 0:
+        if request.args["lang"]=="cn":
+            return render_template("index_cn.html")
+        elif request.args["lang"]=="sp":
+            return render_template("index_sp.html")
+    return render_template("index_en.html")
+
 @app.route("/details",methods=['GET'])
 def details():
-    return render_template("details.html")
+    return render_template("details_en.html")
 
 @app.route("/contact",methods=['GET'])
 def contact():
-    return render_template("contact.html")
+    return render_template("contact_en.html")
 
 @app.route("/location",methods=['GET'])
 def location():
-    return render_template("location.html")
+    return render_template("location_en.html")
     
     
     
